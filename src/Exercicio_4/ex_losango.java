cfe7d53342cc5b44159f438dce05f95ebbddd782
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:44:44 -0300

    Create ex_losango.java

---
+++ src/Exercicio_4/ex_losango.java
@@ -0,0 +1,13 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class ex_losango {
+    public static double calcularArea() {
+   System.out.println("Informe-nos Sobre a Maior Diagonal de seu Losango:")
+                double diag_maior = entrada.nextDouble();
+                System.out.println("Agora, Informe-nos Sobre a Menor Diagonal de seu Losango:");
+                double diag_menor = entrada.nextDouble();
+                System.out.println("A Área de seu Losango Equivale A: " + ((diag_maior * diag_menor)/2) + " cm²!");  
+    }
+}
