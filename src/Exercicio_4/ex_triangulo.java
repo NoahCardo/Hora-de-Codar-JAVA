39c755747c7ab367b4baa6bfa29620b58d374a44
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:49:41 -0300

    Create ex_triangulo.java

---
+++ src/Exercicio_4/ex_triangulo.java
@@ -0,0 +1,13 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class ex_triangulo {
+    public static double calcularArea() {
+   System.out.println("Informe-nos a Base de seu Triângulo:")
+                double base = entrada.nextDouble();
+                System.out.println("Agora, Informe-nos a Altura de seu Triângulo");
+                double altura = entrada.nextDouble();
+                System.out.println("A Área de seu Triângulo Equivale A: " + ((base * altura)/2) + " cm²!");
+    }
+}
