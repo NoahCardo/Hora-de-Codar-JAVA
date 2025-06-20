1ae8b556685ef0330f0fea187a473d9dff35f2e0
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:28:11 -0300

    Create retangulo.java

---
+++ src/Exercício_4/retangulo.java
@@ -0,0 +1,13 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class Retangulo {
+    public static double calcularArea() {
+    System.out.println("Informe-nos a Base de seu Retângulo:");
+                double base = entrada.nextDouble();  //double: Tipo de dado para números decimais de precisão dupla
+                System.out.println("Agora, Informe-nos a Altura de seu Retângulo:");
+                double altura = entrada.nextDouble();
+                System.out.println("A Área de seu Retângulo Equivale A: " + (base * altura) + " cm²!");  
+    }
+}
