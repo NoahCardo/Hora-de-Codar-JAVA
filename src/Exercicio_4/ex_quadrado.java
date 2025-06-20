069ef7f675b21bdcc013ddb1d26f21949bb4070f
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:42:00 -0300

    Create ex_quadrado.java

---
+++ src/Exercicio_4/ex_quadrado.java
@@ -0,0 +1,13 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class ex_quadrado {
+    public static double calcularArea() {
+   System.out.println("Informe-nos Sobre o Primeiro Lado de seu Quadrado:");
+                double lado1 = entrada.nextDouble();
+                System.out.println("Agora, Informe-nos Sobre o Segundo Lado de seu Quadrado:");
+                double lado2 = entrada.nextDouble();
+                System.out.println("A Área de seu Quadrado Equivale A:" + (lado1 * lado2) + " cm²!");
+    }
+}
