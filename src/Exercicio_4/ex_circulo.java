775f0ae0490a791f079f472faf638fa86071b378
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:50:42 -0300

    Create ex_circulo.java

---
+++ src/Exercicio_4/ex_circulo.java
@@ -0,0 +1,11 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class ex_circulo {
+    public static double calcularArea() {
+   System.out.println("Basta nos Informar o Raio do Mesmo! Qual É o Raio de Seu Círculo?")
+                double raio = entrada.nextDouble();
+                System.out.println("A Área de seu Círculo Equivale A: " + (Math.PI * (raio * raio)) + " cm²!");
+    }
+}
