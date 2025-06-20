196cd0163b83fdc8af02dfc784b416a2b6e9561c
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:48:09 -0300

    Create ex_paralelogramo.java

---
+++ src/Exercicio_4/ex_paralelogramo.java
@@ -0,0 +1,13 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class ex_paralelogramo {
+    public static double calcularArea() {
+   System.out.println("Informe-nos a Base de seu Paralelogramo:")
+                double base = entrada.nextDouble();
+                System.out.println("Agora, Informe-nos a Altura de seu Paralelogramo");
+                double altura = entrada.nextDouble();
+                System.out.println("A Área de seu Paralelogramo Equivale A: " + (base * altura) + " cm²!");
+    }
+}
