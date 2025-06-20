2add4be640540de1b5040787b924a85c07d40be1
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-19 22:46:33 -0300

    Create ex_trapezio.java

---
+++ src/Exercicio_4/ex_trapezio.java
@@ -0,0 +1,15 @@
+package Exercicio_4;
+
+import java.util.Scanner;
+
+public class ex_trapezio {
+    public static double calcularArea() {
+   System.out.println("Informe-nos Sobre a Maior Base de seu Trapézio:")
+                double base_maior = entrada.nextDouble();
+                System.out.println("Agora, Informe-nos Sobre a Menor Base de seu Trapézio:");
+                double base_menor = entrada.nextDouble();
+                System.out.println("E, Por Fim, Informe-nos Sobre a Altura de seu Trapézio:");
+                double altura = entrada.nextDouble();
+                System.out.println("A Área de seu Trapézio Equivale A: " + ((base_maior + base_menor) * altura/2) + " cm²!");
+    }
+}
